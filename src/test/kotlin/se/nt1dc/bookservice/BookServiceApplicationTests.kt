f660bbda37426163fec7df97a5fca032520489b3
nt1dc <nt1dcare@gmail.com>
2023-03-17 13:45:49 +0300

    init modules

---
+++ src/test/kotlin/se/nt1dc/bookservice/BookServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package se.nt1dc.bookservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BookServiceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
